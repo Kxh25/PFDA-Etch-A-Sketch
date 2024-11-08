2f56b3d2aa43eeac115fb019739730578891b83c
kylah <kxh230007@utdallas.edu>
2024-11-08 10:40:07 -0600

    created starter code for EtchASketch.py

---
+++ src/EtchASketch.py
@@ -0,0 +1,29 @@
+# how do you write code
+import pygame
+import os
+
+
+
+
+def main():
+
+    """
+    The main function of the script.
+
+    Creates a screen with the design qualities of a digital 
+    Etch-A-Sketch.
+    
+    This function ties all the class functions together to create the screen. The screen
+    should include a canvas, a drawing cursor that can only move within
+    the canvas bounds, a directional knob/button, a size button, a color button,
+    a speed modifier, a save button, and an ESC button.
+    
+     """
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
