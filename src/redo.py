a380990c228cfa174ffcc3c5436aae4ab9b0bb0d
kylah <kxh230007@utdallas.edu>
2024-11-27 11:38:35 -0600

    Trying to redo the whole etchASketch using turtle rather than pygame

---
+++ src/redo.py
@@ -0,0 +1,17 @@
+import turtle
+import os
+
+def main():
+    """draws a line, and affects the size, color, and speed of lines
+     by using different buttons """
+    
+    turtle.setup(700, 500)
+    turtle.title("Etch A Sketch")
+    
+
+
+
+
+
+    if __name__ == "__main__":
+        main()
